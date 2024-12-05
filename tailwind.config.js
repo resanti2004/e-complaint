5eac7a2733bf61344152cc401da230adbe3e6121
Resanti Dwi Cahyani <resantidwicahyani@mail.ugm.ac.id>
2024-12-05 16:42:14 +0700

    ver6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import preline from "preline/plugin";
+import flowbite from "flowbite/plugin";
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/preline/dist/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       colors: {
         primary: "#3731A1", // Warna biru
-        lightPurple: "#A5B4FC", //ungu muda
+        lightPurple: "#A5B4FC", // Ungu muda
       },
       backgroundImage: {
         custom:
@@ -18,21 +21,21 @@ export default {
       },
       width: {
         22: "5.625rem", // 90px
-        15: "3.75rem", //60px
-        97: "29.25rem", //468px
+        15: "3.75rem", // 60px
+        97: "29.25rem", // 468px
       },
       height: {
-        22: "5.625rem", // Example: Custom class w-72
-        19: "4.6875rem", //75px
-        98: "30.3125rem", //485px
+        22: "5.625rem", // 90px
+        19: "4.6875rem", // 75px
+        98: "30.3125rem", // 485px
       },
       maxWidth: {
-        97: "29.25rem", //468px
+        97: "29.25rem", // 468px
       },
       maxHeight: {
-        98: "30.3125rem", //485px
+        98: "30.3125rem", // 485px
       },
     },
   },
-  plugins: [require("preline/plugin")],
+  plugins: [preline, flowbite],
 };
